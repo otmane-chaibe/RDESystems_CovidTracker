5f4cea1e0eadf706dbe33202cd6126102bb32338
Otmane Chaibe <oc35@njit.edu>
2020-10-14 12:04:42 -0400

    Add files via upload

---
+++ src/data/County-data.js
@@ -0,0 +1,203 @@
+export var Countydata = [
+  {
+    Id:"0500000US34017",
+      name: 'Hudson',
+      State: "New Jersey",
+      NewCases:4,
+      ConfirmedCases: 	
+      21342,
+      ConfirmedDeaths: 34
+      
+    
+   },
+   {
+    Id:"0500000US34003",
+    name: 'Bergen',
+    State: "New Jersey",
+    NewCases:4,
+    ConfirmedCases: 23415,
+    ConfirmedDeaths: 34,
+    carbohydrate: 5.5,
+    
+  
+ },
+ {
+  Id:"0500000US34019",  
+  name: 'Hunterdon',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 1505,
+  ConfirmedDeaths: 34,
+  carbohydrate: 5.5,
+  
+
+},
+{
+  Id:"0500000US34025",  
+  name: 'Monmouth',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 12903,
+  ConfirmedDeaths: 34,
+  carbohydrate: 5.5,
+  
+
+},
+{
+  Id:"0500000US34023",
+  name: 'Middlesex',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 20563,
+  ConfirmedDeaths: 34,
+  carbohydrate: 5.5,
+  
+
+},
+{
+  Id:"0500000US34027",  
+  name: 'Morris',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 8298,
+  ConfirmedDeaths: 34,
+  carbohydrate: 5.5,
+  
+
+},
+{
+  Id:"0500000US34037", 
+  name: 'Sussex',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 1615,
+  ConfirmedDeaths: 34,
+  carbohydrate: 5.5,
+  
+
+},
+{
+  Id:"0500000US34013",  
+  name: 'Essex',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 21989,
+  ConfirmedDeaths: 34,
+  carbohydrate: 5.5,
+  
+
+},
+{
+  Id:"0500000US34035",  
+  name: 'Somerset',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 6092,
+  ConfirmedDeaths: 34,
+  carbohydrate: 5.5,
+  
+
+},
+  // // {
+  //   department_id: 17,
+  //   department_name: "Alto Paraguay",
+  //   amount_w: 91.1,
+  //   amount_m: 93.88
+  // },
+  // {
+  //   department_id: 10,
+  //   department_name: "Alto Parana",
+  //   amount_w: 84.57,
+  //   amount_m: 84.92
+  // },
+  // {
+  //   department_id: 13,
+  //   department_name: "Amambay",
+  //   amount_w: 68.97,
+  //   amount_m: 67.57
+  // },
+  // {
+  //   department_id: 16,
+  //   department_name: "Boquerón",
+  //   amount_w: 83.18,
+  //   amount_m: 85.54
+  // },
+  // {
+  //   department_id: 5,
+  //   department_name: "Caaguazu",
+  //   amount_w: 77.55,
+  //   amount_m: 79.49
+  // },
+  // {
+  //   department_id: 6,
+  //   department_name: "Caazapa",
+  //   amount_w: 83.51,
+  //   amount_m: 88.1
+  // },
+  // {
+  //   department_id: 14,
+  //   department_name: "Canindeyu",
+  //   amount_w: 88.64,
+  //   amount_m: 92.04
+  // },
+  // {
+  //   department_id: 11,
+  //   department_name: "Central",
+  //   amount_w: 76.12,
+  //   amount_m: 76.74
+  // },
+  // {
+  //   department_id: 1,
+  //   department_name: "Concepción",
+  //   amount_w: 81.72,
+  //   amount_m: 83.87
+  // },
+  // {
+  //   department_id: 3,
+  //   department_name: "Cordillera",
+  //   amount_w: 86.95,
+  //   amount_m: 88.13
+  // },
+  // {
+  //   department_id: 4,
+  //   department_name: "Guaira",
+  //   amount_w: 80.06,
+  //   amount_m: 81.14
+  // },
+  // {
+  //   department_id: 7,
+  //   department_name: "Itapua",
+  //   amount_w: 71.56,
+  //   amount_m: 73.55
+  // },
+  // {
+  //   department_id: 8,
+  //   department_name: "Misiones",
+  //   amount_w: 87.64,
+  //   amount_m: 88.99
+  // },
+  // {
+  //   department_id: 12,
+  //   department_name: "Ñeembucu",
+  //   amount_w: 77.35,
+  //   amount_m: 76.24
+  // },
+  // {
+  //   department_id: 9,
+  //   department_name: "Paraguari",
+  //   amount_w: 78.68,
+  //   amount_m: 83.33
+  // },
+  // {
+  //   department_id: 15,
+  //   department_name: "Presidente Hayes",
+  //   amount_w: 83.78,
+  //   amount_m: 83.68
+  // },
+  // {
+  //   department_id: 2,
+  //   department_name: "San Pedro",
+  //   amount_w: 90.45,
+  //   amount_m: 93.67
+  // }
+]
