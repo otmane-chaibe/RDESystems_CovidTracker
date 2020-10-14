7858086467a3f6bc277f52e11779a10fc0f1d27d
Otmane Chaibe <oc35@njit.edu>
2020-10-14 12:14:08 -0400

    Add files via upload

---
+++ FrontEnd/src/data/County-data.js
@@ -0,0 +1,96 @@
+export var Countydata = [
+  {
+    Id:"0500000US34017",
+      name: 'Hudson',
+      State: "New Jersey",
+      NewCases:4,
+      ConfirmedCases: 	
+      21342,
+      ConfirmedDeaths: 34
+      
+    
+   },
+   {
+    Id:"0500000US34003",
+    name: 'Bergen',
+    State: "New Jersey",
+    NewCases:4,
+    ConfirmedCases: 23415,
+    ConfirmedDeaths: 34,
+    
+  
+ },
+ {
+  Id:"0500000US34019",  
+  name: 'Hunterdon',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 1505,
+  ConfirmedDeaths: 34,
+ 
+  
+
+},
+{
+  Id:"0500000US34025",  
+  name: 'Monmouth',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 12903,
+  ConfirmedDeaths: 34,
+ 
+  
+
+},
+{
+  Id:"0500000US34023",
+  name: 'Middlesex',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 20563,
+  ConfirmedDeaths: 34,
+ 
+
+},
+{
+  Id:"0500000US34027",  
+  name: 'Morris',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 8298,
+  ConfirmedDeaths: 34,
+ 
+
+},
+{
+  Id:"0500000US34037", 
+  name: 'Sussex',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 1615,
+  ConfirmedDeaths: 34,
+ 
+
+},
+{
+  Id:"0500000US34013",  
+  name: 'Essex',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 21989,
+  ConfirmedDeaths: 34,
+  
+
+},
+{
+  Id:"0500000US34035",  
+  name: 'Somerset',
+  State: "New Jersey",
+  NewCases:4,
+  ConfirmedCases: 6092,
+  ConfirmedDeaths: 34,
+ 
+  
+
+}
+]
