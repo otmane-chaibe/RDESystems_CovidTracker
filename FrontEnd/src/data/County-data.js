51b2d3879686372f9c0fd34ae23e5f1c0b286a8a
Your Name <oc35@njit.edu>
2020-11-08 12:41:09 -0500

    second commit

--- FrontEnd/src/data/County-data.js
+++ FrontEnd/src/data/County-data.js
@@ -3,7 +3,17 @@ export var Countydata = [
     Id:"0500000US34017",
       name: 'Hudson',
       State: "New Jersey",
-      NewCases:4,
+      Recoveries:21342,
+      ConfirmedCases: 	
+      21342,
+      ConfirmedDeaths: 34
+      
+    
+   },{
+    Id:"0500000US36005",
+      name: '5 Boroughs',
+      State: "New York",
+      Recoveries:21342,
       ConfirmedCases: 	
       21342,
       ConfirmedDeaths: 34
@@ -14,7 +24,7 @@ export var Countydata = [
     Id:"0500000US34003",
     name: 'Bergen',
     State: "New Jersey",
-    NewCases:4,
+    Recoveries:4,
     ConfirmedCases: 23415,
     ConfirmedDeaths: 34,
     
@@ -24,7 +34,7 @@ export var Countydata = [
   Id:"0500000US34019",  
   name: 'Hunterdon',
   State: "New Jersey",
-  NewCases:4,
+  Recoveries:21342,
   ConfirmedCases: 1505,
   ConfirmedDeaths: 34,
  
@@ -35,7 +45,7 @@ export var Countydata = [
   Id:"0500000US34025",  
   name: 'Monmouth',
   State: "New Jersey",
-  NewCases:4,
+  Recoveries:30000,
   ConfirmedCases: 12903,
   ConfirmedDeaths: 34,
  
@@ -46,7 +56,7 @@ export var Countydata = [
   Id:"0500000US34023",
   name: 'Middlesex',
   State: "New Jersey",
-  NewCases:4,
+  Recoveries:4,
   ConfirmedCases: 20563,
   ConfirmedDeaths: 34,
  
@@ -56,7 +66,7 @@ export var Countydata = [
   Id:"0500000US34027",  
   name: 'Morris',
   State: "New Jersey",
-  NewCases:4,
+  Recoveries:100,
   ConfirmedCases: 8298,
   ConfirmedDeaths: 34,
  
@@ -66,7 +76,7 @@ export var Countydata = [
   Id:"0500000US34037", 
   name: 'Sussex',
   State: "New Jersey",
-  NewCases:4,
+  Recoveries:1,
   ConfirmedCases: 1615,
   ConfirmedDeaths: 34,
  
@@ -76,7 +86,7 @@ export var Countydata = [
   Id:"0500000US34013",  
   name: 'Essex',
   State: "New Jersey",
-  NewCases:4,
+  Recoveries:400,
   ConfirmedCases: 21989,
   ConfirmedDeaths: 34,
   
@@ -86,7 +96,7 @@ export var Countydata = [
   Id:"0500000US34035",  
   name: 'Somerset',
   State: "New Jersey",
-  NewCases:4,
+  Recoveries:3000,
   ConfirmedCases: 6092,
   ConfirmedDeaths: 34,
  
