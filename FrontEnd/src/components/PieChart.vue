170e6f2cae80c3b734099f5a24c83132af43bb8a
Otmane Chaibe <oc35@njit.edu>
2020-11-08 11:25:15 -0500

    Update PieChart.vue

--- FrontEnd/src/components/PieChart.vue
+++ FrontEnd/src/components/PieChart.vue
@@ -1,47 +1,12 @@
 <script>
-  import { Pie } from 'vue-chartjs'
-import { Countydata } from '../data/County-data'
-  
-var labelsArray = [];
-var amountArray = [];
-for (var i = 0; i < Countydata.length; i++) {
-    labelsArray.push(Countydata[i] ? Countydata[i].name : '');
-    amountArray.push(Countydata[i] ? Countydata[i].ConfirmedCases : '');
-}
-  export default {
+import { Pie } from 'vue-chartjs'
+
+export default {
     extends: Pie,
+    props: ['chartData'],
     data () {
       return {
-        chartData: {
-          labels: labelsArray,
-          datasets: [{
-              borderWidth: 1,
-              borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)' ,    
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)' ,
-              'rgba(75, 192, 192, 1)' ,
-                      
-              ],
-              backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)', 
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(75, 192, 192, 0.2)',                 
-              ],
-              data: amountArray,
-            }]
-        },
+        
         options: {
           legend: {
             display: true
